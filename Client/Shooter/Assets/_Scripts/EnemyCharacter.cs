2105e66825a15aba29baa7fccd1aaf3ef9204c55
Alexander <102901721+Labu1ay@users.noreply.github.com>
2023-07-26 22:56:53 +0300

    First Commit

---
+++ Client/Shooter/Assets/_Scripts/EnemyCharacter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : MonoBehaviour{
+    public void SetPosition(Vector3 position){
+        transform.position = position;
+    }
+}
