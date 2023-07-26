a24e171de43b546ee4d115d737528e73b879cf11
Alexander <102901721+Labu1ay@users.noreply.github.com>
2023-07-27 01:05:33 +0300

    WIP

--- Client/Shooter/Assets/_Scripts/EnemyController.cs
+++ Client/Shooter/Assets/_Scripts/EnemyController.cs
@@ -5,15 +5,19 @@ using Colyseus.Schema;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
+    Vector3 Temp ;
+    Vector3 previousPosition;
     internal void OnChange(List<DataChange> changes) {
         Vector3 position = transform.position;
-
+        // Vector3 previousPosition = transform.position;
         foreach (var dataChange in changes){
             switch(dataChange.Field){
                 case "x":
+                    previousPosition.x = (float)dataChange.PreviousValue;
                     position.x = (float)dataChange.Value;
                     break;
                 case "y":
+                    previousPosition.z = (float)dataChange.PreviousValue;
                     position.z = (float)dataChange.Value;
                     break;
                 default:
@@ -21,6 +25,13 @@ public class EnemyController : MonoBehaviour {
                     break;
             }
         }
-        transform.position = position;
+       
+        //transform.position = position;
+        Temp = Vector3.LerpUnclamped(previousPosition, position, 2f);
+        previousPosition = transform.position;
+       // transform.position = Temp;
+    }
+    private void Update() {
+        transform.position = Vector3.Lerp(transform.position, Temp, Time.deltaTime * 10f);
     }
 }
