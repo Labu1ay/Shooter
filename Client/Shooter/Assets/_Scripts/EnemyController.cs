8f9403ce56b2d6008304eee6d7ddd1f58a2adcb5
Alexander <102901721+Labu1ay@users.noreply.github.com>
2023-07-30 23:03:02 +0300

    WIP Homework Week1

--- Client/Shooter/Assets/_Scripts/EnemyController.cs
+++ Client/Shooter/Assets/_Scripts/EnemyController.cs
@@ -5,33 +5,51 @@ using Colyseus.Schema;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
-    Vector3 Temp ;
-    Vector3 previousPosition;
+    private Vector3 _position;
+    private Vector3 _previousPosition;
+    private Vector3 _newPosition;
+    private Queue<float> _previousPings  = new Queue<float>();
+    private float _time;
+    private void Start() {
+        _position = transform.position;
+        _previousPosition = transform.position;
+        _newPosition = transform.position;
+    }
     internal void OnChange(List<DataChange> changes) {
-        Vector3 position = transform.position;
-        // Vector3 previousPosition = transform.position;
         foreach (var dataChange in changes){
             switch(dataChange.Field){
                 case "x":
-                    previousPosition.x = (float)dataChange.PreviousValue;
-                    position.x = (float)dataChange.Value;
+                    _previousPosition.x = (float)dataChange.PreviousValue;
+                    _position.x = (float)dataChange.Value;
                     break;
                 case "y":
-                    previousPosition.z = (float)dataChange.PreviousValue;
-                    position.z = (float)dataChange.Value;
+                    _previousPosition.z = (float)dataChange.PreviousValue;
+                    _position.z = (float)dataChange.Value;
                     break;
                 default:
                     Debug.Log("Не обрабатывается изменение поля " + dataChange.Field);
                     break;
             }
         }
-       
-        //transform.position = position;
-        Temp = Vector3.LerpUnclamped(previousPosition, position, 2f);
-        previousPosition = transform.position;
-       // transform.position = Temp;
+        AddPing(Time.time - _time);
+        float speed = Vector3.Distance(_position, _previousPosition) / (Time.time - _time);
+        float distanceToNewPosition = speed * GetAveragePing();
+        
+        _newPosition = Vector3.LerpUnclamped(_previousPosition, _position, 1 + distanceToNewPosition);
+        _time = Time.time; 
     }
     private void Update() {
-        transform.position = Vector3.Lerp(transform.position, Temp, Time.deltaTime * 10f);
+            transform.position = Vector3.Lerp(transform.position, _newPosition, 15f * Time.deltaTime);  
     }
+    private void AddPing(float ping){
+        _previousPings.Enqueue(ping);
+        if(_previousPings.Count > 5) _previousPings.Dequeue();
+    }
+    private float GetAveragePing(){
+        float averagePing = 0f;
+        foreach (var ping in _previousPings){
+            averagePing += ping;
+        }
+        return averagePing;
+    } 
 }
